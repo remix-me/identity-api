ef7d2a1c6be35231af20982f49983ad46603782f
Sjef van Leeuwen <36629650+sjefvanleeuwen@users.noreply.github.com>
2018-04-20 22:42:11 +0000

    Add project files.

---
+++ RemixMe.IdentityApi/Controllers/TokenController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace RemixMe.IdentityApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    { 
+        private readonly IConfiguration _config;
+
+        public TokenController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult CreateToken([FromBody]LoginModel login)
+        {
+            IActionResult response = Unauthorized();
+            var user = Authenticate(login);
+
+            if (user != null)
+            {
+                var tokenString = BuildToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string BuildToken(UserModel user)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel Authenticate(LoginModel login)
+        {
+            UserModel user = null;
+
+            if (login.Username == "admin" && login.Password == "admin")
+            {
+                user = new UserModel { Name = "admin", Email = "admin@admin.com" };
+            }
+            return user;
+        }
+
+        public class LoginModel
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        private class UserModel
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+        }
+    }
+}
